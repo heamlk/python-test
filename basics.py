6b428dd633975cfb205eda296c5342d5359faa19
heamlk <mehdiabderrazak60@gmail.com>
2024-07-03 09:56:51 +0530

    hii

--- basics.py
+++
@@ -1,6 +0,0 @@
-import pyjokes
-
-print("Hello ji")
-joke=pyjokes.get_joke()
-print(joke)
-
---
+++ concept/basics.py
@@ -0,0 +1,11 @@
+import pyjokes
+
+print("Hello ji")
+#This prints random joke via pyjokes external module
+"""
+Thankuuuu       multi line comment
+"""
+
+joke=pyjokes.get_joke()
+print(joke)
+
