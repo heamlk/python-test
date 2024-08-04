6a6473d56e2dec5760478120259ed607b05e5656
heamlk <mehdiabderrazak60@gmail.com>
2024-08-04 22:48:22 +0530

    hii

--- Chapter 5/Practise set/spam.py
+++ Chapter 5/Practise set/spam.py
@@ -2,7 +2,7 @@ comment=input()
 
 
 #method_1
-if(comment.find("buy now")>=0 or comment.find("subscribe")>=0):
+if(comment.lower().find("buy now")>=0 or comment.lower().find("subscribe")>=0):
     print("It's a spam")
 else:
     print("Yoo its good")
@@ -11,7 +11,7 @@ else:
 #method_2
 p1="buy now"
 p2="subscribe"
-if((p1 in comment) or (p2 in comment)):
+if((p1 in comment.lower()) or (p2 in comment.lower())):
     print("SPAM")
 else:
     print("Yooo again safe huh")
