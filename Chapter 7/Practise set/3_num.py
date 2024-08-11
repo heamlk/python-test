c81b3b447b2eb8b38fe08f94f4f7b5a36b4c0b21
heamlk <mehdiabderrazak60@gmail.com>
2024-08-11 23:39:58 +0530

    hii

--- Chapter 7/Practise set/3_num.py
+++ Chapter 7/Practise set/3_num.py
@@ -1,1 +1,14 @@
-jddjdj
+def great(a,b,c):
+    if(a>b):
+        if(a>c):
+            print(f"{a} is greatest")
+        else:
+            print(f"{c} is greatest")
+    elif(b>c):
+        print(f"{b} is greatest")
+    else:
+        print(f"{c} is greatest")
+
+
+
+great(int(input()),int(input()),int(input()))
